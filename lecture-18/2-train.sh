9e9dcd4b2375132af624a5bbf876fe1796a9232e
gavin <gavin@desktop.(none)>
2014-10-08 23:17:28 -0400

    I added labs for lecture 18.

---
+++ lecture-18/2-train.sh
@@ -0,0 +1,1 @@
+mrec_train -n4 --input_format tsv --train "splits/joke-ratings.csv.10000.train.*" --outdir models
---
+++ lecture-18/3-predict.sh
@@ -0,0 +1,1 @@
+mrec_predict --input_format tsv --test_input_format tsv --train "splits/joke-ratings.csv.10000.train.*" --modeldir models --outdir recs
